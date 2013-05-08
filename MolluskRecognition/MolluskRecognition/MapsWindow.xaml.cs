c17be27c2f34b2fb21a68935fe3a138341b67810
Lyubov Zvereva <lyubovzvereva@gmail.com>
2013-05-08 13:08:46 +0400

    Задача 3
    Создать стартовое окошко

---
+++ MolluskRecognition/MolluskRecognition/MapsWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MolluskRecognition
+{
+	/// <summary>
+	/// Interaction logic for MapsWindow.xaml
+	/// </summary>
+	public partial class MapsWindow : Window
+	{
+		public MapsWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ MolluskRecognition/MolluskRecognition/StartWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+
+namespace MolluskRecognition
+{
+	/// <summary>
+	/// Interaction logic for StartWindow.xaml
+	/// </summary>
+	public partial class StartWindow : Window
+	{
+		public StartWindow()
+		{
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// Событие клика на кнопку "Каталог НДМ"
+		/// </summary>
+		private void CatalogButton_Click(object sender, RoutedEventArgs e)
+		{
+			CatalogWindow catalog = new CatalogWindow();
+			catalog.Owner = this;
+			catalog.ShowDialog();
+		}
+
+		/// <summary>
+		/// Событие клика на кнопку "Поиск"
+		/// </summary>
+		private void SearchButton_Click(object sender, RoutedEventArgs e)
+		{
+			MainWindow main = new MainWindow();
+			main.ShowDialog();
+		}
+
+		/// <summary>
+		/// Событие клика на кнопку "Карты"
+		/// </summary>
+		private void MapButton_Click(object sender, RoutedEventArgs e)
+		{
+			MapsWindow maps = new MapsWindow();
+			maps.Owner = this;
+			maps.ShowDialog();
+		}
+
+	}
+}
